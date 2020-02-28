63980c0c090b7d031e26f5f953e5d9d314eb2f81
Takuya Sugiyama <sugiyama@supinf.co.jp>
2020-02-28 22:45:48 +0900

    Add buttons to dashboard

--- app/src/main/java/net/satius/keyan/MainActivity.kt
+++ app/src/main/java/net/satius/keyan/MainActivity.kt
@@ -1,12 +1,33 @@
 package net.satius.keyan
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val buttonStartService by lazy { findViewById<Button>(R.id.button_start_service) }
+    private val buttonStopService by lazy { findViewById<Button>(R.id.button_stop_service) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = ("Dashboard")
+
+        setupViews()
     }
+
+    private fun setupViews() {
+        buttonStartService.setOnClickListener { startService() }
+        buttonStopService.setOnClickListener { stopService() }
+    }
+
+    private fun startService() {
+        TODO()
+    }
+
+    private fun stopService() {
+        TODO()
+    }
+
 }
