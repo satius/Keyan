21acb12f20ea0f7157d6a011ba6f8d52a9c57798
Takuya Sugiyama <sugiyama@supinf.co.jp>
2020-03-02 04:14:24 +0900

    add notification wip

--- app/src/main/java/net/satius/keyan/DashboardActivity.kt
+++ app/src/main/java/net/satius/keyan/DashboardActivity.kt
@@ -1,18 +1,35 @@
 package net.satius.keyan
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import net.satius.keyan.notification.keyoperation.KeyOperatableNotification.Companion.CHANNEL_ID
+import net.satius.keyan.notification.keyoperation.KeyOperatableNotificationImpl
 
 class DashboardActivity : AppCompatActivity() {
 
     private val buttonStartService by lazy { findViewById<Button>(R.id.button_start_service) }
     private val buttonStopService by lazy { findViewById<Button>(R.id.button_stop_service) }
 
+    private val notificationManager by lazy { getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
+    private val keyOperatableNotification by lazy { KeyOperatableNotificationImpl(this) } // TODO: DI
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = ("Dashboard")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Sample channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
 
         setupViews()
     }
@@ -23,11 +40,11 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     private fun startService() {
-        TODO()
+        keyOperatableNotification.startForegroundService()
     }
 
     private fun stopService() {
-        TODO()
+        keyOperatableNotification.stopForegroundService()
     }
 
 }
---
+++ app/src/main/java/net/satius/keyan/notification/keyoperation/KeyOperatableNotification.kt
@@ -0,0 +1,146 @@
+package net.satius.keyan.notification.keyoperation
+
+import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.IBinder
+import androidx.annotation.DrawableRes
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import net.satius.keyan.R
+import net.satius.keyan.notification.keyoperation.KeyOperatableNotification.Companion.CHANNEL_ID
+
+interface KeyOperatableNotification {
+    companion object {
+        const val CHANNEL_ID = "KeyanNotification"
+    }
+
+    enum class ActionType(
+        val prop: String,
+        @DrawableRes val iconDrawable: Int,
+        val title: String,
+        val showInCompactView: Boolean
+    ) {
+        LOCK(
+            "lock456787678",
+            R.drawable.ic_launcher_foreground, // TODO: fix
+            "Lock",
+            true
+        ),
+        UNLOCK(
+            "unlock876567897890",
+            R.drawable.ic_launcher_foreground, // TODO: fix
+            "Unlock",
+            true
+        ),
+        REFRESH(
+            "refresh0987890-",
+            R.drawable.ic_launcher_foreground, // TODO: fix
+            "Refresh",
+            false
+        ),
+        LAUNCH(
+            "launch567890-0",
+            R.drawable.ic_launcher_foreground, // TODO: fix
+            "Launch",
+            false
+        )
+    }
+
+    fun startForegroundService()
+
+    fun stopForegroundService()
+
+}
+
+class KeyOperatableNotificationImpl(private val context: Context) : KeyOperatableNotification {
+
+    private var serviceIntent = Intent(context, NotificationService::class.java)
+
+    override fun startForegroundService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent)
+        } else {
+            context.startService(serviceIntent)
+        }
+    }
+
+    override fun stopForegroundService() {
+        context.stopService(serviceIntent)
+    }
+
+    class NotificationService : Service() {
+
+        companion object {
+            const val ID = 1000
+        }
+
+        override fun onBind(p0: Intent?): IBinder? = null
+
+        override fun onCreate() {
+            super.onCreate()
+            startForeground(ID, createNotification())
+        }
+
+        private fun createNotification(): Notification {
+            return NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.sesames) // TODO: fix
+                .setContentTitle("イエノカギ") // TODO
+                .setContentText("Locked/Unlocked") // TODO
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_MIN)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC) // TODO: ユーザ設定
+                .setColor(ContextCompat.getColor(this, R.color.colorAccent))
+                .setColorized(true)
+                .setMediaStyleAndAddActions(
+                    this,
+                    KeyOperatableNotification.ActionType.values().toList()
+                )
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.sesames))
+                .build()
+        }
+
+        private fun NotificationCompat.Builder.addAction(
+            context: Context,
+            type: KeyOperatableNotification.ActionType
+        ) = addAction(
+            type.iconDrawable,
+            type.title,
+            PendingIntent.getBroadcast(
+                context,
+                type.ordinal,
+                Intent(context, KeyOperatableNotificationReceiver::class.java).apply {
+                    action = type.prop
+                },
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        )
+
+        private fun NotificationCompat.Builder.setMediaStyleAndAddActions(
+            context: Context,
+            typeList: List<KeyOperatableNotification.ActionType>
+        ): NotificationCompat.Builder {
+
+            val actionArray = mutableListOf<Int>().apply {
+                typeList.forEachIndexed { idx, it ->
+                    if (it.showInCompactView) {
+                        add(idx)
+                    }
+                }
+            }.toIntArray()
+
+            setStyle(
+                androidx.media.app.NotificationCompat
+                    .MediaStyle()
+                    .setShowActionsInCompactView(*actionArray)
+            )
+
+            typeList.forEach { addAction(context, it) }
+            return this
+        }
+    }
+}
